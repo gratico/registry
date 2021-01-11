4f1f761e62c8295af2dab8b3e55c55357355672d
Abhishiv <abhishiv@gmail.com>
2021-01-11 23:29:58 +0100

    commit

--- src/builder.js
+++ src/builder.js
@@ -1,11 +1,16 @@
 import { build, bundle, download, isDownloaded, ensureDownload, getFolderRoot } from './utils.js'
 ;(async () => {
-  const pkg = {
-    name: process.env.NPM_PACKAGE_NAME,
-    version: process.env.NPM_PACKAGE_VERSION,
-    main: process.env.NPM_PACKAGE_MAIN
+  try {
+    const pkg = {
+      name: process.env.NPM_PACKAGE_NAME,
+      version: process.env.NPM_PACKAGE_VERSION,
+      main: process.env.NPM_PACKAGE_MAIN
+    }
+    console.log(pkg)
+    await build(pkg)
+    await bundle(pkg, pkg.main)
+  } catch (e) {
+    console.error(e)
+    process.exit(1)
   }
-  console.log(pkg)
-  await build(pkg)
-  await bundle(pkg, pkg.main)
 })()
